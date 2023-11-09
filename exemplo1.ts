6d9e9ad119e197a459006dcb7ec87f95e44cc995
Daniela Felipe Soares <117787402+Danifeares@users.noreply.github.com>
2023-11-09 19:20:57 -0300

    Update exemplo1.ts
    Signed-off-by: Daniela Felipe Soares <117787402+Danifeares@users.noreply.github.com>

--- exemplo1.ts
+++ exemplo1.ts
@@ -4,10 +4,12 @@ type Usuario = {
     email: string
 }
 
+// tipo utilitário Required: obriga que todos os dados tipados em Usuario sejam passados.
 // const cadastrarUsuario = (usuario: Required<Usuario>) => {
 //     return usuario;
 // }
 
+// tipo utilitário Partial: permite que os dados sejam passados parcialmente, apenas um nome, apenas o nome e a idade, nada, etc
 // const cadastrarUsuario = (usuario: Partial<Usuario>) => {
 //     return usuario;
 // }
@@ -16,6 +18,7 @@ type Usuario = {
 //     nome: 'Guido'
 // })
 
+// Readonly seta as informações como somente leitura, não consigo alterar posteriormente o que foi setado aqui.
 const guido: Readonly<Usuario> = {
     nome: 'Guido',
     email: 'guido@email.com'
