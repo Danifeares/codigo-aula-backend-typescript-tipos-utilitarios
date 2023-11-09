2c8b5ec6e5f7699f7a49ad013733e2ef50231f27
Daniela Felipe Soares <117787402+Danifeares@users.noreply.github.com>
2023-11-09 19:13:44 -0300

    Update exemplo4.ts
    Signed-off-by: Daniela Felipe Soares <117787402+Danifeares@users.noreply.github.com>

--- exemplo4.ts
+++ exemplo4.ts
@@ -4,4 +4,4 @@
 type Ex1 = Exclude<'a' | 'b' | 'c', 'a' | 'b'>;
 type Ex2 = Extract<'a' | 'b' | 'c', 'a'>;
 type Ex3 = Exclude<boolean | string | number, number>;
-type Ex4 = Extract<number | number[], number>;
+type Ex4 = Extract<number | number[], number>; 
