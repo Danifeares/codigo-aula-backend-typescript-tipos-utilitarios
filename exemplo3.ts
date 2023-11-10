01b8526ac33644a6df70cf097ac9ac9a4de40909
Daniela Felipe Soares <117787402+Danifeares@users.noreply.github.com>
2023-11-10 14:23:45 -0300

    Update exemplo3.ts
    Signed-off-by: Daniela Felipe Soares <117787402+Danifeares@users.noreply.github.com>

--- exemplo3.ts
+++ exemplo3.ts
@@ -10,11 +10,13 @@ type Pessoa = {
 type NovoUsuario = Pick<Pessoa, 'nome' | 'idade'>;
 type NovoUsuario2 = Omit<Pessoa, 'email'>
 
+// o pick permite setar apenas o que eu quero usar do tipo Pessoa, nesse caso foi setado nome e idade.
 const guido2: NovoUsuario = {
     nome: 'guido',
     idade: 32
 }
 
+// o omit permite omitir algo, nesse caso foo omitido email do tipo pessoa, então posso usar essa tipagem sem passar o email obrigatoriamente.
 const guido3: NovoUsuario2 = {
     nome: 'Guido',
     idade: 32
