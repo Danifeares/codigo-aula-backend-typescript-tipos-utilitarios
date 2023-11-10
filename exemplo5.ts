64aeb4c0b60fc9932dbd6bddf747b21f7b7fdf24
Daniela Felipe Soares <117787402+Danifeares@users.noreply.github.com>
2023-11-10 14:38:31 -0300

    Update exemplo5.ts
    Signed-off-by: Daniela Felipe Soares <117787402+Danifeares@users.noreply.github.com>

--- exemplo5.ts
+++ exemplo5.ts
@@ -1,3 +1,4 @@
 // NonNullable<type>
-
+// serve para remover null e/ou undefined de tipos que tem propriedades null e/ou undefined. 
 type Ex5 = NonNullable<number | string[] | null | undefined>;
+// nesse caso, ex5 irá ter apenas number e string[] como tipagem.
